c1befc68edfdedc662220e730c4398f714c429b2
Chris Chen <chrischanyedu@gmail.com>
2023-10-27 13:12:37 -0400

    Test for pull
    Test for pull

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ielts_tools import WordTrainer
 
 
 if __name__ == "__main__":
+    # Test for pull
     # Training based on UK accent
     UK_Trainer = WordTrainer(1)
     UK_Trainer.easilyUse()
